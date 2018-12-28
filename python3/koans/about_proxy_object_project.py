069feda38002d22988d119dee789e8446d65a36e
Joep Schuurkes <j19sch@gmail.com>
2018-12-28 16:36:53 +0100

    proxy objects, regex

--- python3/koans/about_proxy_object_project.py
+++ python3/koans/about_proxy_object_project.py
@@ -21,11 +21,36 @@ from runner.koan import *
 class Proxy:
     def __init__(self, target_object):
         # WRITE CODE HERE
+        self._messages = []
 
         #initialize '_obj' attribute last. Trust me on this!
         self._obj = target_object
 
-    # WRITE CODE HERE
+    def __getattr__(self, attr_name):
+        self._messages.append(attr_name)
+        return self._obj.__getattribute__(attr_name)
+
+    def __setattr__(self, attr_name, value):
+        attrs = ['_obj', '_messages']
+        if attr_name in attrs:
+            object.__setattr__(self, attr_name, value)
+        else:
+            self._messages.append(attr_name)
+            object.__setattr__(self._obj, attr_name, value)
+   
+    def messages(self):
+        return self._messages
+
+    def was_called(self, attr_name):
+        if attr_name in self._messages:
+            return True
+        else:
+            return False
+
+    def number_of_times_called(self, attr_name):
+        return self._messages.count(attr_name)
+
+
 
 # The proxy object should pass the following Koan:
 #
